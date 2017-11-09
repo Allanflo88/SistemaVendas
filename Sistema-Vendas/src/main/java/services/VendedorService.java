e15ac416adddb3731c04eadb3ffad6b8fc1f3954
Caique Siqueira <caique.siqueira@fatec.sp.gov.br>
2017-11-09 12:57:25 -0200

    Review class VendedorService

--- Sistema-Vendas/src/main/java/services/VendedorService.java
+++ Sistema-Vendas/src/main/java/services/VendedorService.java
@@ -2,59 +2,58 @@ package services;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
 import entity.Vendedor;
 
-public class VendedorService {
-	private Vendedor vendedor = new Vendedor();
-	private List<Vendedor> vendedores = new ArrayList<>();
+public class VendedorService extends Service {
 	
-	protected static EntityManagerFactory emf = Persistence.createEntityManagerFactory("SistemaVendas");
-	
-	public Vendedor getVendedor() {
-		return vendedor;
-	}
-	public void setVendedor(Vendedor vendedor) {
-		this.vendedor = vendedor;
-	}
+	@SuppressWarnings("unchecked")
 	public List<Vendedor> getVendedores() {
 		EntityManager em = emf.createEntityManager();
-		Query q = em.createQuery("select * from Vendedor", Vendedor.class);
-		vendedores = q.getResultList();
-		return vendedores;
-	}
-	public void setVendedores(List<Vendedor> vendedores) {
-		this.vendedores = vendedores;
+		try {
+			Query q = em.createQuery("select * from Vendedor", Vendedor.class);
+			return q.getResultList();
+		} finally {
+			em.close();
+		}
 	}
 	
-	public void Salvar(Vendedor vendedor) {
+	public void salvar(Vendedor vendedor) {
 		EntityManager em = emf.createEntityManager();
-		em.getTransaction().begin();
-		em.persist(vendedor);
-		em.getTransaction().commit();
-		em.close();
+		try {
+			em.getTransaction().begin();
+			em.persist(vendedor);
+			em.getTransaction().commit();
+		} finally {
+			em.close();
+		}
 	}
+	
 	public void excluir(Vendedor vendedor) {
-		Vendedor rmv;
 		EntityManager em = emf.createEntityManager();
-		em.getTransaction().begin();
-		rmv = em.find(Vendedor.class, vendedor.getCpf());
-		em.remove(rmv);
-		em.getTransaction().commit();
-		em.close();
-		
+		try {
+			Vendedor rmv;
+			em.getTransaction().begin();
+			rmv = em.find(Vendedor.class, vendedor.getCpf());
+			em.remove(rmv);
+			em.getTransaction().commit();
+		} finally {
+			em.close();
+		}
 	}
 	
 	public void atualizar(Vendedor vendedor) {;
 		EntityManager em = emf.createEntityManager();
-		em.getTransaction().begin();
-		em.merge(vendedor);
-		em.getTransaction().commit();
-		em.close();
+		try {
+			em.getTransaction().begin();
+			em.merge(vendedor);
+			em.getTransaction().commit();
+		} finally {
+			em.close();
+		}
 	}
 
 }
