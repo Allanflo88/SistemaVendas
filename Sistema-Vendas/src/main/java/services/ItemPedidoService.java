28ac66612e85ce3c4d87809fec6bcd389194a2d0
Caique Siqueira <caique.siqueira@fatec.sp.gov.br>
2017-11-09 12:27:35 -0200

    Review class ItemPedidoService

--- Sistema-Vendas/src/main/java/services/ItemPedidoService.java
+++ Sistema-Vendas/src/main/java/services/ItemPedidoService.java
@@ -1,60 +1,57 @@
 package services;
 
-import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import javax.persistence.Query;
 import entity.ItemPedido;
 import entity.ItemPedidoID;
 
-public class ItemPedidoService {
-	private ItemPedido itemPedido = new ItemPedido();
-	private List<ItemPedido> itemPedidos = new ArrayList<>();
-	
-	protected static EntityManagerFactory emf = Persistence.createEntityManagerFactory("SistemaVendas");
+public class ItemPedidoService extends Service {
 
-	
-	public ItemPedido getItemPedido() {
-		return itemPedido;
-	}
-	public void setItemPedido(ItemPedido itemPedido) {
-		this.itemPedido = itemPedido;
-	}
+	@SuppressWarnings("unchecked")
 	public List<ItemPedido> getItemPedidos() {
 		EntityManager em = emf.createEntityManager();
-		Query q = em.createQuery("select * from ItemPedido", ItemPedido.class);
-		itemPedidos = q.getResultList();
-		return itemPedidos;
+		try {
+			Query q = em.createQuery("select * from ItemPedido", ItemPedido.class);
+			return q.getResultList();
+		} finally {
+			em.close();
+		}
 	}
-	public void setItemPedidos(List<ItemPedido> itemPedidos) {
-		this.itemPedidos = itemPedidos;
-	}
-	public void Salvar(ItemPedido itemPedido) {
+	
+	public void salvar(ItemPedido itemPedido) {
 		EntityManager em = emf.createEntityManager();
-		em.getTransaction().begin();
-		em.persist(itemPedido);
-		em.getTransaction().commit();
-		em.close();
+		try {
+			em.getTransaction().begin();
+			em.persist(itemPedido);
+			em.getTransaction().commit();
+		} finally {
+			em.close();
+		}
 	}
+	
 	public void excluir(ItemPedidoID itemPedido) {
-		ItemPedido rmv;
 		EntityManager em = emf.createEntityManager();
-		em.getTransaction().begin();
-		rmv = em.find(ItemPedido.class, itemPedido);
-		em.remove(rmv);
-		em.getTransaction().commit();
-		em.close();
-		
+		try {
+			ItemPedido rmv;
+			em.getTransaction().begin();
+			rmv = em.find(ItemPedido.class, itemPedido);
+			em.remove(rmv);
+			em.getTransaction().commit();
+		} finally {
+			em.close();
+		}
 	}
 	
 	public void atualizar(ItemPedido itemPedido) {;
 		EntityManager em = emf.createEntityManager();
-		em.getTransaction().begin();
-		em.merge(itemPedido);
-		em.getTransaction().commit();
-		em.close();
+		try {
+			em.getTransaction().begin();
+			em.merge(itemPedido);
+			em.getTransaction().commit();
+		} finally {
+			em.close();
+		}
 	}
 	
 
