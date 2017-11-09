3e88e35ac655ef3dbeab3a018a23aa9e6a8e3e82
Caique Siqueira <caique.siqueira@fatec.sp.gov.br>
2017-11-09 12:21:38 -0200

    Review classes ClienteService and ClienteMB

--- Sistema-Vendas/src/main/java/services/ClienteService.java
+++ Sistema-Vendas/src/main/java/services/ClienteService.java
@@ -1,63 +1,57 @@
 package services;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import javax.persistence.Query;
 import entity.Cliente;
 
-
-public class ClienteService {
-	private Cliente cliente = new Cliente();
-	private List<Cliente> clientes = new ArrayList<>();
+public class ClienteService extends Service {
 	
-	protected static EntityManagerFactory emf = Persistence.createEntityManagerFactory("SistemaVendas");
-
-	
-	public Cliente getCliente() {
-		return cliente;
-	}
-	public void setCliente(Cliente cliente) {
-		this.cliente = cliente;
-	}
+	@SuppressWarnings("unchecked")
 	public List<Cliente> getClientes() {
 		EntityManager em = emf.createEntityManager();
-		Query q = em.createQuery("select * from Cliente", Cliente.class);
-		clientes = q.getResultList();
-		return clientes;
+		try {
+			Query q = em.createQuery("select * from Cliente", Cliente.class);
+			return q.getResultList();
+		} finally {
+			em.close();
+		}
 	}
-	public void setClientes(List<Cliente> clientes) {
-		this.clientes = clientes;
-	}
-	public void Salvar(Cliente cliente) {
+	
+	public void salvar(Cliente cliente) {
 		EntityManager em = emf.createEntityManager();
-		em.getTransaction().begin();
-		em.persist(cliente);
-		em.getTransaction().commit();
-		em.close();
+		try {
+			em.getTransaction().begin();
+			em.persist(cliente);
+			em.getTransaction().commit();
+		} finally {
+			em.close();
+		}
 	}
+	
 	public void excluir(Cliente cliente) {
-		Cliente rmv;
 		EntityManager em = emf.createEntityManager();
-		em.getTransaction().begin();
-		rmv = em.find(Cliente.class, cliente.getCpf());
-		em.remove(rmv);
-		em.getTransaction().commit();
-		em.close();
-		
+		try {
+			Cliente rmv;
+			em.getTransaction().begin();
+			rmv = em.find(Cliente.class, cliente.getCpf());
+			em.remove(rmv);
+			em.getTransaction().commit();
+		} finally {
+			em.close();
+		}
 	}
 	
-	public void atualizar(Cliente cliente) {;
+	public void atualizar(Cliente cliente) {
 		EntityManager em = emf.createEntityManager();
-		em.getTransaction().begin();
-		em.merge(cliente);
-		em.getTransaction().commit();
-		em.close();
+		try {
+			em.getTransaction().begin();
+			em.merge(cliente);
+			em.getTransaction().commit();
+		} finally {
+			em.close();
+		}
 	}
 	
-	
-
 }
