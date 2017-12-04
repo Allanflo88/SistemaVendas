107dca67fe979ec10eef69a8e3df357d9275a25f
Allanflo88 <allanfloresdejesus@hotmail.com>
2017-12-04 12:44:58 -0200

    update RealizarPedido.xhtml

--- Sistema-Vendas/src/main/java/entity/Pedido.java
+++ Sistema-Vendas/src/main/java/entity/Pedido.java
@@ -1,13 +1,11 @@
 package entity;
 import java.io.Serializable;
-import java.util.ArrayList;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 
 
 @Entity
