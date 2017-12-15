4339d826dc0694f127de5cf415930d3bcc2faaf6
Allanflo88 <allanfloresdejesus@hotmail.com>
2017-12-14 23:49:34 -0200

    Update Realizar pedido

--- Sistema-Vendas/src/main/java/managedbean/PedidoMB.java
+++ Sistema-Vendas/src/main/java/managedbean/PedidoMB.java
@@ -2,10 +2,9 @@ package managedbean;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-import javax.faces.context.FacesContext;
+
 
 import org.primefaces.event.RowEditEvent;
 
